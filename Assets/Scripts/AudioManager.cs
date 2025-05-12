f00f1423aa8814f60035dc8b2d2a483fe4d90968
Luís Gustavo <githlima3105@gmail.com>
2025-05-12 14:21:12 -0300

    SpeedUP Interaction

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -15,15 +15,25 @@ public class AudioManager : MonoBehaviour
     public AudioClip touchImage;
     public AudioClip groundFall;
 
+    public float normalPitch = 1f;
+    public float speedUpPitch = 1.5f;
+
     private void Start() 
     {
         audioSource.clip = background;
+        audioSource.pitch = normalPitch;
+        audioSource.loop = true;
         audioSource.Play();
     }
 
     public void PlaySFX(AudioClip clip)
     {
         SFXSource.PlayOneShot(clip);
+    }
+
+    public void SetPitch(float isSpeedUp)
+    {
+        audioSource.pitch = isSpeedUp;
     }
 
 
