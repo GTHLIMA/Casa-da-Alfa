443f650c4210ae0b3d278c1d416dad0c5719db6b
Luís Gustavo <githlima3105@gmail.com>
2025-05-23 17:06:29 -0300

    UI Elements && Fixes

--- Assets/Scripts/WaitButton.cs
+++ Assets/Scripts/WaitButton.cs
@@ -2,34 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class WaitButton : MonoBehaviour
 {
-    public GameObject waitButton;
+    public GameObject PauseMenu;
     public GameObject playButton;
+    public GameObject slingshot;
+
 
     AudioManager audioManager;
-    public float waitTime = 5f; 
-
     private void Awake()
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
-    void Start()
-    {
-        waitButton.SetActive(false); 
-        StartCoroutine(WaitAndActivateButton());
-        
-    }
-
-    private IEnumerator WaitAndActivateButton()
-    {
-        yield return new WaitForSeconds(waitTime); 
-        waitButton.SetActive(true); 
-    }
 
     public void hidePlayButton()
     {
-        playButton.SetActive(false); 
+        playButton.SetActive(false);
+        audioManager.PlayAudio(audioManager.background);
+    }
+    public void hidePlayButton1()
+    {
+        playButton.SetActive(false);
+        slingshot.SetActive(true);
         audioManager.PlayAudio(audioManager.background);
     }
 
