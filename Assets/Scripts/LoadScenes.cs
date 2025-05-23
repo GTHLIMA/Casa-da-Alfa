28c1fbb6308a5e3ed2594bc6e3cfd3b6f319889e
Luís Gustavo <githlima3105@gmail.com>
2025-05-23 20:29:06 -0300

    Change Font Letter

--- Assets/Scripts/LoadScenes.cs
+++ Assets/Scripts/LoadScenes.cs
@@ -6,10 +6,7 @@ using UnityEngine.SceneManagement;
 public class LoadScenes : MonoBehaviour
 {
     public void MainMenu() => SceneManager.LoadScene("MainMenu");
-    public void LoadLevels() => SceneManager.LoadScene("Levels");
     public void Level1_1() => SceneManager.LoadScene("Game1");
     public void Level1_2() => SceneManager.LoadScene("Game1.2");
-
-
 
 }
