378546114b62680d6cd5d16a851104d6057da6ab
Caio Lúcio <kaiolucioalmeida@gmail.com>
2025-05-25 16:48:07 -0300

    ADC das animações das mãos e do texto flutuante

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,8 +18,8 @@ public class GameManager : MonoBehaviour
     public float spawnRate;
 
     [Header("rarity settings")]
-    [Range(0f, 1f)] // serve para definir o valor entre 0% e 100%
-    public float rareChance = 0.2f; // 20% de chance de spawn 
+    [Range(0f, 1f)] 
+    public float rareChance = 0.2f; 
     public Material goldenMaterial;
     public int rareScore = 50;
 
@@ -139,7 +139,7 @@ public class GameManager : MonoBehaviour
     public void BombTouch()
     {
     Debug.Log("Bomba Tocou! Lógica de fim de jogo ou perda de vida vai aqui.");
-    bombTouchCount++; // Atualiza a contagem (que você já tinha)
+    bombTouchCount++; 
     }
     public void AddScore(int amount)
     {
@@ -179,9 +179,9 @@ public class GameManager : MonoBehaviour
         bool isRare = UnityEngine.Random.value < rareChance;
         SpriteRenderer sr = instance.GetComponent<SpriteRenderer>();
 
-        if (isRare && sr != null && goldenMaterial != null)
+         if (isRare && sr != null && goldenMaterial != null)
         {
-            Debug.Log("SPAWNOU UM RARO! Pontos: " + rareScore); // <-- DEBUG
+            Debug.Log("SPAWNOU UM RARO! Pontos: " + rareScore);
             sr.material = goldenMaterial;
 
             ParticleSystem ps = instance.GetComponentInChildren<ParticleSystem>(true);
@@ -190,16 +190,12 @@ public class GameManager : MonoBehaviour
                 ps.Play();
             }
 
-            // --- NOVO: Avisa o script Bomb que ele é raro e vale mais ---
+           
             Bomb bombScript = instance.GetComponent<Bomb>();
             if (bombScript != null)
             {
-                bombScript.SetAsRare(rareScore); // Chama o novo método e passa os 50 pontos
-                GameManager.Instance.AddScore(rareScore); // Adiciona os pontos ao GameManager
-               
-                
+                bombScript.SetAsRare(rareScore); // apenas define o valor, nao adiciona score aq
             }
-            // --- FIM DO NOVO ---
         }
 
         Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
