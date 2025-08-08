53055057b68d45a44a1de0dfc1c6a78dc15ff862
Luís Gustavo <githlima3105@gmail.com>
2025-08-08 13:13:10 -0300

    lvls

--- Assets/Scripts/LoadScenes.cs
+++ Assets/Scripts/LoadScenes.cs
@@ -8,8 +8,14 @@ public class LoadScenes : MonoBehaviour
     public void MainMenu() => SceneManager.LoadScene("MainMenu");
     // public void LoadLevels() => SceneManager.LoadScene("Levels");
     public void Level1() => SceneManager.LoadScene("Game1");
-    public void Level2() => SceneManager.LoadScene("Game2");
     public void Level1_3() => SceneManager.LoadScene("Game1.3");
+    public void Level2() => SceneManager.LoadScene("Game2");
+
+    public void Level2_1() => SceneManager.LoadScene("Game2.1");
+    
+    public void Level2_2() => SceneManager.LoadScene("Game2.3");
+    
+    public void Level2_3() => SceneManager.LoadScene("Game2.3");
 
 
 
