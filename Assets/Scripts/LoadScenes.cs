e70ffbd7b1ff55da3ff366e489aa004d2e544ab4
Caio Lúcio <kaiolucioalmeida@gmail.com>
2025-08-29 09:41:47 -0300

    Adição do jogo 3 e 3.1

--- Assets/Scripts/LoadScenes.cs
+++ Assets/Scripts/LoadScenes.cs
@@ -1,23 +1,54 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadScenes : MonoBehaviour
 {
-    public void MainMenu() => SceneManager.LoadScene("MainMenu");
-    // public void LoadLevels() => SceneManager.LoadScene("Levels");
-    public void Level1() => SceneManager.LoadScene("Game1");
-    public void Level1_3() => SceneManager.LoadScene("Game1.3");
-    public void Level2() => SceneManager.LoadScene("Game2");
+    public void MainMenu() => LoadSceneWithOrientation("MainMenu", false);
+    public void Level1() => LoadSceneWithOrientation("Game1", false);
+    public void Level1_3() => LoadSceneWithOrientation("Game1.3", false);
+    public void Level2() => LoadSceneWithOrientation("Game2", true);
+    public void Level2_1() => LoadSceneWithOrientation("Game2.1", true);
+    public void Level2_2() => LoadSceneWithOrientation("Game2.2", true);
+    public void Level3() => LoadSceneWithOrientation("Game3", true);
+    public void Level3_1() => LoadSceneWithOrientation("Game3.1", true);
+    public void placeholder() => LoadSceneWithOrientation("LandScape", true);
 
-    public void Level2_1() => SceneManager.LoadScene("Game2.1");
-    
-    public void Level2_2() => SceneManager.LoadScene("Game2.3");
-    
-    public void Level2_3() => SceneManager.LoadScene("Game2.3");
-    public void Level3() => SceneManager.LoadScene("Game3");
+    private void LoadSceneWithOrientation(string sceneName, bool isLandscape)
+    {
+        // Definir orientação antes de carregar
+        if (isLandscape)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+            Screen.autorotateToLandscapeLeft = true;
+            Screen.autorotateToLandscapeRight = false;
+            Screen.autorotateToPortraitUpsideDown = false;
+            Screen.autorotateToPortrait = false;
+        }
+
+        else
+        {
+            Screen.orientation = ScreenOrientation.Portrait;
+            Screen.autorotateToLandscapeLeft = false;
+            Screen.autorotateToLandscapeRight = false;
+            Screen.autorotateToPortraitUpsideDown = false;
+            Screen.autorotateToPortrait = false;
+        }
+
+        // vai carregar a cena e forçar update da UI
+        StartCoroutine(LoadAndFixUI(sceneName));
+    }
+
+    private IEnumerator LoadAndFixUI(string sceneName)
+    {
+
+        SceneManager.LoadScene(sceneName);
+        yield return null;
 
 
+        Canvas.ForceUpdateCanvases();
 
+        yield return null;
+        Canvas.ForceUpdateCanvases();
+    }
 }
