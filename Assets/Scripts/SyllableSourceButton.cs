ffc3136ac618618f947941c9702927fa53ef8074
Caio Lúcio <kaiolucioalmeida@gmail.com>
2025-09-13 12:49:17 -0300

    update game 3
    adição de uma nova logica onde a criança terá mais interatividade com as palavras. Mudança dos scripts de silabas e de respostas, alteração da ordem de palavras

--- Assets/Scripts/SyllableSourceButton.cs
+++ Assets/Scripts/SyllableSourceButton.cs
@@ -4,57 +4,71 @@ using UnityEngine.UI;
 public class SyllableSourceButton : MonoBehaviour
 {
     private SyllablePuzzleManager manager;
-    // Trocado para public para que o manager possa ler
-    public SyllablePuzzleManager.SourceWordData myData { get; private set; }
+    private SyllablePuzzleManager.OnScreenWord myData;
     private Button button;
     private Image image;
-    private Image silabaReveladaImage;
+    private Image revealedSyllableImage;
 
     private void Awake()
     {
-        Transform silabaReveladaTransform = transform.Find("SilabaRevelada");
-        if (silabaReveladaTransform != null)
+        // Procura por um filho com o nome exato "SilabaRevelada"
+        Transform revealedSyllableTransform = transform.Find("SilabaRevelada");
+        if (revealedSyllableTransform != null)
         {
-            silabaReveladaImage = silabaReveladaTransform.GetComponent<Image>();
+            revealedSyllableImage = revealedSyllableTransform.GetComponent<Image>();
+        }
+        else
+        {
+            Debug.LogError("ERRO DE PREFAB: Não foi possível encontrar um filho chamado 'SilabaRevelada' no prefab " + gameObject.name);
         }
     }
 
-    public void Setup(SyllablePuzzleManager.SourceWordData data, SyllablePuzzleManager managerRef)
+    public void Setup(SyllablePuzzleManager.OnScreenWord data, SyllablePuzzleManager managerRef)
     {
         myData = data;
         manager = managerRef;
         image = GetComponent<Image>();
-        image.sprite = data.sourceDrawing;
+        image.sprite = data.drawingImage;
 
         button = GetComponent<Button>();
         button.onClick.RemoveAllListeners();
         button.onClick.AddListener(OnClicked);
 
-        if (silabaReveladaImage != null)
+        if (revealedSyllableImage != null)
         {
-            silabaReveladaImage.sprite = data.firstSyllableImage;
-            silabaReveladaImage.gameObject.SetActive(false);
+            revealedSyllableImage.sprite = data.syllableImage;
+            revealedSyllableImage.gameObject.SetActive(false);
         }
     }
 
     private void OnClicked()
     {
-        if (silabaReveladaImage != null)
+      
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+        if(audioManager != null)
         {
-            silabaReveladaImage.gameObject.SetActive(true);
+          
+            audioManager.PauseAudio(audioManager.background);
         }
-        
+        // ------------------------------------
+
         if (manager != null)
         {
-            manager.PlaySyllableAudio(myData.firstSyllableAudio);
-            SetUsed(true);
             manager.OnSourceButtonClicked(myData, this);
+        }
+    }
+
+    // CORREÇÃO: Adicionado o método público que o manager precisa para comandar a revelação.
+    public void RevealLocalSyllable()
+    {
+        if (revealedSyllableImage != null)
+        {
+            revealedSyllableImage.gameObject.SetActive(true);
         }
     }
 
     public void SetUsed(bool used)
     {
         button.interactable = !used;
-        image.color = used ? new Color(0.7f, 0.7f, 0.7f, 0.8f) : Color.white;
     }
 }
