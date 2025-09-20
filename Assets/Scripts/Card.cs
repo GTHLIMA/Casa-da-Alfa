f7404959d3e8ed09306bad03bd4abb301cf9d855
Caio Lúcio <kaiolucioalmeida@gmail.com>
2025-09-20 11:29:37 -0300

    Remodelagem do jogo 4

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,47 +1,91 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using PrimeTween;
 
 public class Card : MonoBehaviour
-{   
+{
     [SerializeField] private Image iconImage;
+    [SerializeField] private AudioClip cardAudio;
+
+    private AudioSource audioSource;
 
     public Sprite hiddenIconSprite;
     public Sprite iconSprite;
-    public bool isSelected;
-    
-    public CardsController controller;
 
+    public bool isSelected = true;
+
+    [HideInInspector] public CardsController controller;
+
+    private void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
+
+
+    public void Initialize(Sprite sp, AudioClip clip, CardsController ctrl)
+    {
+        iconSprite = sp;
+        cardAudio = clip;
+        controller = ctrl;
+
+        if (iconImage != null && hiddenIconSprite != null)
+            iconImage.sprite = hiddenIconSprite;
+
+        isSelected = true;
+
+        transform.localEulerAngles = Vector3.zero;
+
+        var btn = GetComponent<Button>();
+        if (btn != null)
+        {
+            btn.onClick.RemoveAllListeners();
+            btn.onClick.AddListener(OnCardClick);
+        }
+    }
 
     public void OnCardClick()
     {
-        controller.SetSelected(this);   
+        controller.SetSelected(this);
     }
 
     public void Show()
-    {      
+    {
         Tween.Rotation(transform, new Vector3(0f, 180f, 0f), 0.2f);
-        Tween.Delay(0.1f, () => iconImage.sprite = iconSprite);
+        Tween.Delay(0.1f, () => { if (iconImage != null) iconImage.sprite = iconSprite; });
         isSelected = false;
     }
 
     public void Hide()
     {
-        Tween.Rotation(transform,
-         new Vector3(0f, 0f, 0f), 0.2f);
-
-        Tween.Delay(0.1f, () => {
-
-        iconImage.sprite = hiddenIconSprite;
-        isSelected = true;
+        Tween.Rotation(transform, new Vector3(0f, 0f, 0f), 0.2f);
+        Tween.Delay(0.1f, () =>
+        {
+            if (iconImage != null && hiddenIconSprite != null) iconImage.sprite = hiddenIconSprite;
+            isSelected = true; 
         });
     }
 
-    public void SetIconSprite(Sprite sp)
+    public void SetAudio(AudioClip clip) => cardAudio = clip;
+
+    public void PlayAudio(System.Action onFinished)
     {
-        iconSprite = sp;
+        if (cardAudio != null)
+        {
+            audioSource.clip = cardAudio;
+            audioSource.Play();
+            controller.StartCoroutine(WaitForAudio(onFinished));
+        }
+        else
+        {
+            onFinished?.Invoke();
+        }
+    }
+
+    private IEnumerator WaitForAudio(System.Action onFinished)
+    {
+        yield return new WaitWhile(() => audioSource.isPlaying);
+        onFinished?.Invoke();
     }
 }
-
