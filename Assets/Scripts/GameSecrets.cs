56380e4c596e96289ad58177677a9481659b1eef
Luís Gustavo <githlima3105@gmail.com>
2025-12-06 14:06:56 -0300

    update

---
+++ Assets/Scripts/GameSecrets.cs
@@ -0,0 +1,5 @@
+public static class GameSecrets
+{
+    // COLE SUA CHAVE AQUI DENTRO DAS ASPAS
+    public const string OPENAI_KEY = "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"; 
+}
